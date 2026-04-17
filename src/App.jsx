03d134ae98737a406bb2fa728598cd64ec00d145
moonjung <jekang0123@gmail.com>
2026-04-17 09:00:55 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-500">
+      Tailwind 연결 성공!
+    </h1>
+  )
+}
+
+export default App
